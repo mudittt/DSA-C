77808cc60162c593088aa521fc232c6d0e8b3112
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-08 15:34:42 +0530

    pop

--- stack-List/stack-List.c
+++ stack-List/stack-List.c
@@ -32,17 +32,46 @@ int isFull(struct stack *ptr)
     }
 }
 
+void push(struct stack *ptr, int data)
+{
+    if (isFull(ptr))
+    {
+        printf("stack overflow.");
+    }
+    else
+    {
+        ptr->arr[ptr->top + 1] = data;
+        ptr->top++;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    /* struct stack s;
+    /*   struct stack s;
      s.size = 5;
      s.top = -1;
-     s.arr = (int *)malloc(s.size * (sizeof(int)));  */
+     s.arr = (int *)malloc(s.size * (sizeof(int)));    */
     printf("Hi..\n");
     struct stack *s;
     s->size = 5;
     s->top = -1;
     s->arr = (int *)malloc(s->size * (sizeof(int)));
+
+    /*    printf("\npushing an element manually in the stack..\n");
+    s->arr[0] = 7;
+    s->top++;    */
+
+    printf("\nchecking if stack is empty or not...\n");
+    if ((isEmpty(s)) == 1)
+    {
+        printf("The stack is empty . ");
+    }
+    else
+    {
+        printf("The stack is not empty . ");
+    }
+
+    push(s, 55);
 
     printf("\nchecking if stack is empty or not...\n");
     if ((isEmpty(s)) == 1)
