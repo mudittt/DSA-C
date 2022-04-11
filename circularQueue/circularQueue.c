9b1b9978e9224294c4124f8cb24cba80085af2a4
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-11 17:16:22 +0530

    changed values of f & b and added enqueue func

--- circularQueue/circularQueue.c
+++ circularQueue/circularQueue.c
@@ -27,13 +27,26 @@ int isEmpty(struct circularQueue *q)
     return 0;
 }
 
+void enqueue(struct circularQueue *q, int data)
+{
+    if (isFull(q))
+    {
+        printf("\nQueue is full. Wait for your turn.\n");
+    }
+    else
+    {
+        q->b = (q->b + 1) % q->size;
+        q->arr[q->b] = data;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     printf("\nHi.\n");
     struct circularQueue *q;
     q->size = 10;
-    q->f = -1;
-    q->b = -1;
+    q->f = 0;
+    q->b = 0;
     q->arr = (int *)malloc((sizeof(int)) * (q->size));
     return 0;
 }
