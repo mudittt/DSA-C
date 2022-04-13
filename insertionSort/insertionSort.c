24d11bfde3c0af4cae0477a0f118b5d375a945f3
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-13 22:10:22 +0530

    copied code from bubblesort

---
+++ insertionSort/insertionSort.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+void insertionSort(int *str)
+{
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("\nhi.\n");
+    int random[6] = {11, 4, 17, 9, 0, 7};
+
+    BubbleSort(random);
+    for (int i = 0; i < 6; i++)
+    {
+        printf("-> %d ", random[i]);
+    }
+
+    return 0;
+}
