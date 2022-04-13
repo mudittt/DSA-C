9dcaabb16a140f66335a0184f9b72149558013c7
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-13 18:32:25 +0530

    tried the algo for a diff array

--- bubbleSort/bubbleSort.c
+++ bubbleSort/bubbleSort.c
@@ -25,7 +25,7 @@ void BubbleSort(int *str)
 int main(int argc, char const *argv[])
 {
     printf("\nhi.\n");
-    int random[6] = {11, 4, 17, 9, 2, 7};
+    int random[6] = {11, 4, 17, 9, 0, 7};
 
     BubbleSort(random);
     for (int i = 0; i < 6; i++)
