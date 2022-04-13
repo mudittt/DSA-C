32803dd35921b98dc17bba35c09bbed6bc46951e
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-13 16:34:03 +0530

    added bubble sort func

--- bubbleSort/bubbleSort.c
+++ bubbleSort/bubbleSort.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
+void BubbleSort(int *str[6])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (*str[i] == *str[i + 1])
+        {
+            i++;
+        }
+        else
+        {
+            int temp = (*str[i + 1]);
+            (*str[i + 1]) = (*str[i]);
+            (*str[i]) = temp;
+        }
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     printf("\nhi.\n");
-    int random[6] = {2, 4, 7, 9, 11, 17};
+    int random[6] = {11, 4, 17, 9, 2, 7};
+
+    BubbleSort(random);
+    for (int i = 0; i < 6; i++)
+    {
+        printf("-> %d ", random[i]);
+    }
 
     return 0;
 }
