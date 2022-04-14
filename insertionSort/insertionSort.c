e9cddbbbbdfec568a4bc6a082ef2a4be3d0d53a0
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-14 11:44:53 +0530

    added conditions and PRINTFs everywhere

--- insertionSort/insertionSort.c
+++ insertionSort/insertionSort.c
@@ -3,7 +3,29 @@
 
 void insertionSort(int *str)
 {
-    
+    int a = 1, j;
+    while (a != 6)
+    {
+
+        for (j = a; j > 0; j--)
+        {
+            printf("j=%d and j-1=%d\n", j, j - 1);
+            if (str[j] < str[j - 1])
+            {
+                printf("before:: str[j-1]=%d and str[j]=%d\n", str[j - 1], str[j]);
+                int temp = str[j - 1];
+                str[j - 1] = str[j];
+                str[j] = temp;
+                printf("after:: str[j-1]=%d and str[j]=%d\n", str[j - 1], str[j]);
+            }
+            else
+            {
+                break;
+            }
+        }
+        a++;
+        printf("a=%d\n", a);
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -11,7 +33,7 @@ int main(int argc, char const *argv[])
     printf("\nhi.\n");
     int random[6] = {11, 4, 17, 9, 0, 7};
 
-    BubbleSort(random);
+    insertionSort(random);
     for (int i = 0; i < 6; i++)
     {
         printf("-> %d ", random[i]);
