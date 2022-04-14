07e81161e606120390110c5aeb1df6d59d6da2bc
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-14 11:49:15 +0530

    alloted memory to q,  dequeue func & traverse func

--- circularQueue/circularQueue.c
+++ circularQueue/circularQueue.c
@@ -11,7 +11,7 @@ struct circularQueue
 
 int isFull(struct circularQueue *q)
 {
-    if ((q->b + 1) % (q->size) == (q->f))
+    if (((q->b + 1) % (q->size)) == (q->f))
     {
         return 1;
     }
@@ -20,7 +20,7 @@ int isFull(struct circularQueue *q)
 
 int isEmpty(struct circularQueue *q)
 {
-    if (q->f == q->b)
+    if ((q->f) == (q->b))
     {
         return 1;
     }
@@ -35,18 +35,53 @@ void enqueue(struct circularQueue *q, int data)
     }
     else
     {
-        q->b = (q->b + 1) % q->size;
+        q->b = ((q->b + 1) % (q->size));
         q->arr[q->b] = data;
+        // printf("\nSuccessful enqueue\n");
+    }
+}
+
+void dequeue(struct circularQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("\nQueue is already empty.\n");
+    }
+    else
+    {
+        q->f = ((q->f + 1) % (q->size));
+    }
+}
+
+void queueTraversal(struct circularQueue *q)
+{
+    if (isEmpty(q))
+    {
+        printf("\nQueue is empty.\n");
+    }
+    else
+    {
+        for (int i = ((q->f) + 1); i <= (q->b); i++)
+        {
+            printf("-> %d\n", q->arr[i]);
+        }
     }
 }
 
 int main(int argc, char const *argv[])
 {
     printf("\nHi.\n");
-    struct circularQueue *q;
+    struct circularQueue *q = (struct circularQueue *)malloc(sizeof(struct circularQueue));
     q->size = 10;
     q->f = 0;
     q->b = 0;
     q->arr = (int *)malloc((sizeof(int)) * (q->size));
+
+    printf("\nhehe\n");
+    enqueue(q, 55);
+    enqueue(q, 34);
+    dequeue(q);
+    queueTraversal(q);
     return 0;
 }
+// print nhi horahi
