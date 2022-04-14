40970216a2964eabb5636e2bb9d449936c49a829
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-14 22:24:41 +0530

    added the algo, working fine!

--- MergeSort/MergeSort.c
+++ MergeSort/MergeSort.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
-void MergeSort(int *str, int *str2, int *str3)
+void MergeSort(int *str1, int *str2, int *str3, int m, int n)
 {
+    int i, j, k;
+    i = j = k = 0;
+    while (i < m && j < n)
+    {
+        if (str1[i] < str2[j])
+        {
+            str3[k] = str1[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            str3[k] = str2[j];
+            j++;
+            k++;
+        }
+    }
+    while (i < m)
+    {
+        str3[k] = str1[i];
+        i++;
+        k++;
+    }
+    while (j < n)
+    {
+        str3[k] = str2[j];
+        j++;
+        k++;
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -15,19 +44,19 @@ int main(int argc, char const *argv[])
     printf("\nBefore merging first array ->\n");
     for (int i = 0; i < 6; i++)
     {
-        printf("-> %d ", random1[i]);
+        printf("~%d ", random1[i]);
     }
     printf("\nBefore merging second array ->\n");
     for (int i = 0; i < 8; i++)
     {
-        printf("-> %d ", random2[i]);
+        printf("~%d ", random2[i]);
     }
 
-    // MergeSort(random1, random2, sorted);
-    // printf("\nAfter merging ->\n");
-    // for (int i = 0; i < 6; i++)
-    // {
-    //     printf("-> %d ", random3[i]);
-    // }
+    MergeSort(random1, random2, sorted, 6, 8);
+    printf("\n\nAfter merging ->\n");
+    for (int i = 0; i < 14; i++)
+    {
+        printf("~%d ", sorted[i]);
+    }
     return 0;
 }
