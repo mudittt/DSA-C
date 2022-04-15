0d8833f82715c9624f6135fde9fc2a800df9c52b
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-15 18:30:17 +0530

    created a f( ) for creating a node wid NULLpointer

--- BinaryTreeLinked/BinaryTreeLinked.c
+++ BinaryTreeLinked/BinaryTreeLinked.c
@@ -8,10 +8,23 @@ struct node
     struct node *right;
 };
 
+struct node *createNode(int data)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+    // returning the created node
+}
+
 int main(int argc, char const *argv[])
 {
     printf("hi\n");
-    // Constructing the root node
+    /*
+
+    // Steps are repeatative here, so creating a exclusive func instead
+    //  Constructing the root node
     struct node *r = (struct node *)malloc(sizeof(struct node));
     r->data = 2;
     r->left = NULL;
@@ -32,5 +45,29 @@ int main(int argc, char const *argv[])
     // Linked -> root node with p1 and p2
     r->left = NULL;
     r->right = NULL;
+
+    */
+
+    // Using function
+    struct node *r = createNode(2);
+    struct node *p1 = createNode(1);
+    struct node *p2 = createNode(4);
+
+    r->left = NULL;
+    r->right = NULL;
+
+    /*
+                 ________
+                |       |
+                |   2   |
+                |_______|
+                /       \
+     ________  /        \_________
+    |       |/          |        |
+    |   1   |           |   4    |
+    |_______|           |________|
+
+
+    */
     return 0;
 }
