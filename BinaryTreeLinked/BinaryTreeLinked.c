1e715e7e71e5472fba7f61b05e75267e89121b95
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-15 16:33:59 +0530

    created a node structure

---
+++ BinaryTreeLinked/BinaryTreeLinked.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+int main(int argc, char const *argv[])
+{
+    printf("hi\n");
+    return 0;
+}
