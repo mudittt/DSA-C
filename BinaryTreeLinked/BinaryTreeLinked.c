d94d050bd2e05b91b91c5d4b1c2f2b18291a099e
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-15 16:48:34 +0530

    created the root node with its two children

--- BinaryTreeLinked/BinaryTreeLinked.c
+++ BinaryTreeLinked/BinaryTreeLinked.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <malloc.h>
 
 struct node
 {
@@ -11,5 +11,26 @@ struct node
 int main(int argc, char const *argv[])
 {
     printf("hi\n");
+    // Constructing the root node
+    struct node *r = (struct node *)malloc(sizeof(struct node));
+    r->data = 2;
+    r->left = NULL;
+    r->right = NULL;
+
+    // First child of root
+    struct node *p1 = (struct node *)malloc(sizeof(struct node));
+    p1->data = 1;
+    p1->left = NULL;
+    p1->right = NULL;
+
+    // Second child of root
+    struct node *p2 = (struct node *)malloc(sizeof(struct node));
+    p2->data = 4;
+    p2->left = NULL;
+    p2->right = NULL;
+
+    // Linked -> root node with p1 and p2
+    r->left = NULL;
+    r->right = NULL;
     return 0;
 }
