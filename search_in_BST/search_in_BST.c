d715b976d6a4cde8b9c7be78ad9ba9cfba29bf97
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-16 12:42:48 +0530

    created a tree using createNode( )

---
+++ search_in_BST/search_in_BST.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *n = (struct node *)malloc(sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+    // returning the created node
+}
+
+int main()
+{
+    printf("hi\n");
+    int str[6];
+    struct node *r = createNode(6);    // root
+    struct node *pl = createNode(4);   // parent left
+    struct node *pll = createNode(2);  // left child of left parent
+    struct node *plr = createNode(5);  // right child of left parent
+    struct node *pr = createNode(8);   // parent left
+    struct node *prr = createNode(10); // right child of right parent
+
+    // root to parent
+    r->left = pl;
+    r->right = pr;
+
+    // Left parent to child
+    pl->left = pll;
+    pl->right = plr;
+
+    // Right parent to child
+    pr->right = prr;
+
+    return 0;
+}
