25cb44525cb8e04d2bd6fe14a3a3bb80bcdaeecb
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-18 21:48:49 +0530

    func for creating node and finding balance factor

--- AVL Tree/rotation.c
+++ AVL Tree/rotation.c
@@ -52,6 +52,30 @@ int FindHeight(struct node *n)
     return n->height;
 }
 
+struct node *createNode(int data)
+{
+    printf("hi.\n");
+    struct node *haha = (struct node *)malloc(sizeof(struct node));
+    haha->key = data;
+    haha->left = NULL;
+    haha->right = NULL;
+    haha->height = 1;
+    return haha;
+}
+
+int balanceFactor(struct node *n)
+{
+    if (n == NULL)
+    {
+        return 0;
+    }
+    else if (FindHeight(n->left) > FindHeight(n->right))
+    {
+        return FindHeight(n->left) - FindHeight(n->right);
+    }
+    return FindHeight(n->right) - FindHeight(n->left);
+}
+
 int main()
 {
 
